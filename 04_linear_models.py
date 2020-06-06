a83d57c84ca73280190304dea40225c5a8fffd14
brown5628 <brown5628@gmail.com>
2020-06-06 09:54:12 -0400

    Finish ch 4

--- 04_linear_models.py
+++ 04_linear_models.py
@@ -1,12 +1,13 @@
 # %%
-import arviz as az 
-import matplotlib.pyplot as plt 
+import arviz as az
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd 
-import pymc3 as pm 
-import scipy.stats as stats 
+import pandas as pd
+import pymc3 as pm
+import scipy.stats as stats
 import altair as alt
-from scipy.interpolate import griddata 
+from scipy.interpolate import griddata
+from patsy import dmatrix
 
 # %%
 pos = np.random.uniform(-1, 1, size=(16, 1000)).sum(0)
@@ -32,7 +33,7 @@ plt.xlabel("p")
 plt.ylabel("Density")
 
 # %%
-d= pd.read_csv("data/Howell1.csv", sep=";", header=0)
+d = pd.read_csv("data/Howell1.csv", sep=";", header=0)
 d.head()
 
 # %%
@@ -45,37 +46,22 @@ d.height
 d2 = d[d.age >= 18]
 
 # %%
-d2['height'].plot(kind='hist')
+d2["height"].plot(kind="hist")
 
 # %%
-alt.Chart(d2).mark_bar().encode(
-    alt.X("height:Q", bin=True),
-    y='count()'
-)
+alt.Chart(d2).mark_bar().encode(alt.X("height:Q", bin=True), y="count()")
 
 # %%
 x = np.linspace(100, 250, 100)
-source = pd.DataFrame({
-    'x': x,
-    'f(x)': stats.norm.pdf(x, 178, 20)
-})
+source = pd.DataFrame({"x": x, "f(x)": stats.norm.pdf(x, 178, 20)})
 
-alt.Chart(source).mark_line().encode(
-    x='x',
-    y='f(x)'
-)
+alt.Chart(source).mark_line().encode(x="x", y="f(x)")
 
 # %%
-x = np.linspace(-10, 60, 100) 
-source = pd.DataFrame({
-    'x': x,
-    'f(x)': stats.uniform.pdf(x, 0, 50)
-})
+x = np.linspace(-10, 60, 100)
+source = pd.DataFrame({"x": x, "f(x)": stats.uniform.pdf(x, 0, 50)})
 
-alt.Chart(source).mark_line().encode(
-    x='x',
-    y='f(x)'
-)
+alt.Chart(source).mark_line().encode(x="x", y="f(x)")
 
 # %%
 n_samples = 10000
@@ -87,24 +73,24 @@ plt.xlabel("heights")
 plt.yticks([])
 
 # %%
-sample_mu = stats.norm.rvs(loc=178, scale =100, size=n_samples)
+sample_mu = stats.norm.rvs(loc=178, scale=100, size=n_samples)
 prior_h = stats.norm.rvs(loc=sample_mu, scale=sample_sigma)
 az.plot_kde(prior_h)
 plt.xlabel("heights")
 plt.yticks([])
 
 # %%
-post = np.mgrid[150:160:0.05, 7:9:0.05].reshape(2, -1).T 
+post = np.mgrid[150:160:0.05, 7:9:0.05].reshape(2, -1).T
 
 likelihood = [
-    sum(stats.norm.logpdf(d2.height, loc=post[:, 0][i], scale = post[:, 1][i]))
+    sum(stats.norm.logpdf(d2.height, loc=post[:, 0][i], scale=post[:, 1][i]))
     for i in range(len(post))
 ]
 
 post_prod = (
     likelihood
     + stats.norm.logpdf(post[:, 0], loc=178, scale=20)
-    + stats.uniform.logpdf(post[:, 1], loc=0, scale =50)
+    + stats.uniform.logpdf(post[:, 1], loc=0, scale=50)
 )
 post_prob = np.exp(post_prod - max(post_prod))
 
@@ -116,3 +102,375 @@ zi = griddata((post[:, 0], post[:, 1]), post_prob, (xi[None, :], yi[:, None]))
 plt.contour(xi, yi, zi)
 
 # %%
+_, ax = plt.subplots()
+ax.imshow(zi, origin="bottom")
+ax.set_xticks([0, 50, 100])
+ax.set_xticklabels([140, 150, 160])
+ax.set_yticks([0, 50, 100])
+ax.set_yticks([0, 50, 100])
+ax.set_yticklabels([4, 6.5, 9])
+ax.grid(False)
+
+# %%
+sample_rows = np.random.choice(
+    np.arange(len(post)), size=10000, replace=True, p=(post_prob / post_prob.sum())
+)
+sample_mu = post[:, 0][sample_rows]
+sample_sigma = post[:, 1][sample_rows]
+
+plt.plot(sample_mu, sample_sigma, "o", alpha=0.05)
+plt.axis("equal")
+plt.grid(False)
+plt.xlabel("sample_mu")
+plt.ylabel("sample_sigma")
+
+# %%
+_, ax = plt.subplots(1, 2, figsize=(8, 4))
+az.plot_kde(sample_mu, ax=ax[0])
+ax[0].set_xlabel("sample_mu")
+ax[0].set_yticks([])
+az.plot_kde(sample_sigma, ax=ax[1])
+ax[1].set_xlabel("sample_sigma")
+ax[1].set_yticks([])
+
+# %%
+az.hpd(sample_mu), az.hpd(sample_sigma)
+
+# %%
+d3 = np.random.choice(d2.height, 20)
+post2 = np.mgrid[150:170:0.1, 4:20:0.1].reshape(2, -1).T
+
+likelihood2 = [
+    sum(stats.norm.logpdf(d3, loc=post2[:, 0][i], scale=post2[:, 1][i]))
+    for i in range(len(post2))
+]
+
+post_prod2 = (
+    likelihood2
+    + stats.norm.logpdf(post2[:, 0], loc=178, scale=20)
+    + stats.uniform.logpdf(post2[:, 1], loc=0, scale=50)
+)
+
+post_prob2 = np.exp(post_prod2 - max(post_prod2))
+
+sample_rows2 = np.random.choice(
+    np.arange(len(post2)), size=10000, replace=True, p=(post_prob2 / post_prob2.sum())
+)
+
+sample_mu2 = post2[:, 0][sample_rows2]
+sample_sigma2 = post2[:, 1][sample_rows2]
+
+# %%
+plt.plot(sample_mu2, sample_sigma2, "o", alpha=0.05)
+plt.axis("equal")
+plt.xlabel("sample_mu2")
+plt.ylabel("sample_sigma2")
+plt.grid(False)
+
+# %%
+az.plot_kde(sample_sigma2)
+plt.xlabel("sample_sigma2")
+plt.yticks([])
+
+# %%
+d = pd.read_csv("data/Howell1.csv", sep=";", header=0)
+d2 = d[d.age >= 18]
+
+# %%
+with pm.Model() as m4_1:
+    mu = pm.Normal("mu", mu=178, sd=20)
+    sigma = pm.Uniform("sigma", lower=0, upper=50)
+    height = pm.Normal("height", mu=mu, sd=sigma, observed=d2.height)
+
+# %%
+with m4_1:
+    trace_4_1 = pm.sample(1000, tune=1000)
+
+# %%
+az.plot_trace(trace_4_1)
+
+# %%
+az.summary(trace_4_1, round_to=2, kind="stats")
+
+# %%
+with pm.Model() as m4_1:
+    mu = pm.Normal("mu", mu=178, sd=20, testval=d2.height.mean())
+    sigma = pm.Uniform("sigma", lower=0, upper=50, testval=d2.height.std())
+    height = pm.Normal("height", mu=mu, sd=sigma, observed=d2.height)
+    trace_4_1 = pm.sample(1000, tune=1000)
+
+# %%
+with pm.Model() as m4_2:
+    mu = pm.Normal("mu", mu=178, sd=0.1)
+    sigma = pm.Uniform("sigma", lower=0, upper=50)
+    height = pm.Normal("height", mu=mu, sd=sigma, observed=d2.height)
+    trace_4_2 = pm.sample(1000, tune=1000)
+az.summary(trace_4_2, round_to=2, kind="stats")
+
+# %%
+trace_df = pm.trace_to_dataframe(trace_4_1)
+trace_df.cov()
+
+# %%
+np.diag(trace_df.cov())
+
+# %%
+trace_df.corr()
+
+# %%
+trace_df.head()
+
+# %%
+trace_4_1["sigma"][:10]
+
+# %%
+az.summary(trace_4_1, round_to=2, kind="stats")
+
+# %%
+stats.multivariate_normal.rvs(mean=trace_df.mean(), cov=trace_df.cov(), size=10)
+
+# %%
+plt.plot(d2.height, d2.weight, ".")
+
+# %%
+alt.Chart(d2).mark_circle(size=60).encode(x="height", y="weight")
+
+# %%
+np.random.seed(2971)
+N = 100  # 100 lines
+a = stats.norm.rvs(178, 20, N)
+b = stats.norm.rvs(0, 10, N)
+
+# %%
+_, ax = plt.subplots(1, 2, sharey=True)
+xbar = d2.weight.mean()
+for i in range(N):
+    ax[0].plot(a[i] + b[i] * (x - xbar), "k", alpha=0.2)
+    ax[0].set_xlim(d2.weight.min(), d2.weight.max())
+    ax[0].set_ylim(-100, 400)
+    ax[0].axhline(0, c="k", ls="--")
+    ax[0].axhline(272, c="k")
+    ax[0].set_xlabel("weight")
+    ax[0].set_ylabel("height")
+
+b = stats.lognorm.rvs(s=1, scale=1, size=100)
+for i in range(N):
+    ax[1].plot(a[i] + b[i] * (x - xbar), "k", alpha=0.2)
+    ax[1].set_xlim(d2.weight.min(), d2.weight.max())
+    ax[1].set_ylim(-100, 400)
+    ax[1].axhline(0, c="k", ls="--", label="embryo")
+    ax[1].axhline(272, c="k")
+    ax[1].set_xlabel("weight")
+    ax[1].text(x=35, y=282, s="World's tallest person (272cm)")
+    ax[1].text(x=35, y=-25, s="Embryo")
+
+# %%
+with pm.Model() as m4_3:
+    a = pm.Normal("a", mu=178, sd=20)
+    b = pm.Normal("b", mu=0, sd=1)
+    sigma = pm.Uniform("sigma", 0, 50)
+    mu = a + np.exp(b) * (d2.weight - xbar)
+    height = pm.Normal("height", mu=mu, sd=sigma, observed=d2.height)
+    trace_4_3b = pm.sample(1000, tune=1000)
+
+# %%
+with pm.Model() as m4_3b:
+    a = pm.Normal("a", mu=178, sd=20)
+    b = pm.Lognormal("b", mu=0, sd=1)
+    sigma = pm.Uniform("sigma", 0, 50)
+    mu = a + b * (d2.weight - xbar)
+    height = pm.Normal("height", mu=mu, sd=sigma, observed=d2.height)
+    trace_4_3 = pm.sample(1000, tune=1000)
+
+# %%
+az.summary(trace_4_3, kind="stats")
+
+# %%
+trace_4_3_df = pm.trace_to_dataframe(trace_4_3)
+trace_4_3_df.cov().round(3)
+
+# %%
+plt.plot(d2.weight, d2.height, ".")
+plt.plot(d2.weight, trace_4_3["a"].mean() + trace_4_3["b"].mean() * (d2.weight - xbar))
+plt.xlabel(d2.columns[1])
+plt.ylabel(d2.columns[0])
+
+# %%
+trace_4_3_df.head(5)
+
+# %%
+N = [10, 50, 150, 352][0]
+dN = d2[:N]
+with pm.Model() as m_N:
+    a = pm.Normal("a", mu=178, sd=100)
+    b = pm.Lognormal("b", mu=0, sd=1)
+    sigma = pm.Uniform("sigma", 0, 50)
+    mu = pm.Deterministic("mu", a + b * (dN.weight - dN.weight.mean()))
+    height = pm.Normal("height", mu=mu, sd=sigma, observed=dN.height)
+    trace_N = pm.sample(1000, tune=1000)
+
+# %%
+plt.plot(dN.weight, dN.height, "C0o")
+idxs = np.random.randint(len(trace_N), size=20)
+for idx in idxs:
+    plt.plot(
+        dN.weight,
+        trace_N["a"][idx] + trace_N["b"][idx] * (dN.weight - dN.weight.mean()),
+        "C1-",
+        alpha=0.5,
+    )
+plt.xlabel(d2.columns[1])
+plt.ylabel(d2.columns[0])
+
+# %%
+mu_at_50 = trace_4_3["a"] + trace_4_3["b"] * (50 - d2.weight.mean())
+
+# %%
+az.plot_kde(mu_at_50)
+plt.xlabel("heights")
+plt.yticks([])
+
+# %%
+az.hpd(mu_at_50)
+
+# %%
+weight_seq = np.arange(25, 71)
+trace_4_3_thinned = trace_4_3[::10]
+mu_pred = np.zeros((len(weight_seq), len(trace_4_3_thinned) * trace_4_3.nchains))
+for i, w in enumerate(weight_seq):
+    mu_pred[i] = trace_4_3_thinned["a"] + trace_4_3_thinned["b"] * (
+        w - d2.weight.mean()
+    )
+
+# %%
+plt.plot(weight_seq, mu_pred, "C0.", alpha=0.1)
+plt.xlabel("weight")
+plt.ylabel("height")
+
+# %%
+mu_mean = mu_pred.mean(1)
+mu_hpd = az.hpd(mu_pred.T)
+
+# %%
+plt.scatter(d2.weight, d2.height)
+plt.plot(weight_seq, mu_mean, "k")
+az.plot_hpd(weight_seq, mu_pred.T)
+plt.xlabel("weight")
+plt.ylabel("height")
+plt.xlim(d2.weight.min(), d2.weight.max())
+
+# %%
+height_pred = pm.sample_posterior_predictive(trace_4_3, 20000, m4_3)
+
+# %%
+height_pred_hpd = az.hpd(height_pred["height"])
+
+# %%
+plt.scatter(d2.weight, d2.height)
+az.plot_hpd(weight_seq, mu_pred.T)
+az.plot_hpd(d2.weight, height_pred["height"])
+plt.plot(weight_seq, mu_mean, "k")
+plt.xlabel("weight")
+plt.ylabel("height")
+plt.xlim(d2.weight.min(), d2.weight.max())
+
+# %%
+d["weight_std"] = (d.weight - d.weight.mean()) / d.weight.std()
+d["weight_std2"] = d.weight_std ** 2
+
+with pm.Model() as m_4_5:
+    a = pm.Normal("a", mu=178, sd=100)
+    b1 = pm.Lognormal("b1", mu=0, sd=1)
+    b2 = pm.Normal("b2", mu=0, sd=1)
+    sigma = pm.Uniform("sigma", lower=0, upper=50)
+    mu = pm.Deterministic("mu", a + b1 * d.weight_std + b2 * d.weight_std2)
+    height = pm.Normal("height", mu=mu, sd=sigma, observed=d.height)
+    trace_4_5 = pm.sample(1000, tune=1000)
+
+# %%
+varnames = ["~mu"]
+az.plot_trace(trace_4_5, varnames)
+
+# %%
+az.summary(trace_4_5, varnames, kind="stats", round_to=2)
+
+# %%
+mu_pred = trace_4_5["mu"]
+height_pred = pm.sample_posterior_predictive(trace_4_5, 200, m_4_5)
+
+# %%
+plt.scatter(d.weight_std, d.height, c="C0", alpha=0.3)
+az.plot_hpd(d.weight_std, mu_pred)
+az.plot_hpd(d.weight_std, height_pred["height"])
+
+# %%
+weight_m = np.vstack((d.weight_std, d.weight_std ** 2, d.weight_std ** 3))
+
+# %%
+with pm.Model() as m_4_6:
+    a = pm.Normal("a", mu=178, sd=100)
+    b = pm.Normal("b", mu=0, sd=10, shape=3)
+    sigma = pm.Uniform("sigma", lower=0, upper=50)
+    mu = pm.Deterministic("mu", a + pm.math.dot(b, weight_m))
+    height = pm.Normal("height", mu=mu, sd=sigma, observed=d.height)
+    trace_4_6 = pm.sample(1000, tune=100)
+
+# %%
+mu_pred = trace_4_6["mu"]
+height_pred = pm.sample_posterior_predictive(trace_4_6, 200, m_4_6)
+
+plt.scatter(d.weight_std, d.height, c="C0", alpha=0.3)
+az.plot_hpd(d.weight_std, mu_pred)
+az.plot_hpd(d.weight_std, height_pred["height"])
+
+at = np.arange(-2, 3)
+plt.xticks(at, np.round(at * d.weight.std() + d.weight.mean(), 1))
+
+# %%
+d = pd.read_csv("data/cherry_blossoms.csv")
+az.summary(d.dropna().to_dict(orient="list"), kind="stats")
+
+# %%
+d2 = d.dropna(subset=["doy"])
+num_knots = 15
+knot_list = np.quantile(d2.year, np.linspace(0, 1, num_knots))
+
+# %%
+B = dmatrix(
+    "bs(year, knots=knots, degree=3, include_intercept=True) - 1",
+    {"year": d2.year.values, "knots": knot_list[1:-1]},
+)
+
+# %%
+_, ax = plt.subplots(1, 1, figsize=(12, 4))
+for i in range(17):
+    ax.plot(d2.year, (B[:, i]), color="C0")
+ax.set_xlabel("year")
+ax.set_ylabel("basis")
+
+# %%
+with pm.Model() as m4_7:
+    a = pm.Normal("a", 100, 10)
+    w = pm.Normal("w", mu=0, sd=10, shape=B.shape[1])
+    mu = pm.Deterministic("mu", a + pm.math.dot(np.asarray(B, order="F"), w.T))
+    # mu = pm.Deterministic("mu", a + pm.math.dot(B.base, w.T))
+    sigma = pm.Exponential("sigma", 1)
+    D = pm.Normal("D", mu, sigma, observed=d2.doy)
+    trace_m4_7 = pm.sample(1000)
+
+# %%
+
+_, ax = plt.subplots(1, 1, figsize=(12, 4))
+wp = trace_m4_7[w].mean(0)
+for i in range(17):
+    ax.plot(d2.year, (wp[i] * B[:, i]), color="C0")
+ax.set_xlim(812, 2015)
+ax.set_ylim(-6, 6)
+
+# %%
+_, ax = plt.subplots(1, 1, figsize=(12, 4))
+
+ax.plot(d2.year, d2.doy, "o", alpha=0.3)
+az.plot_hpd(d2.year, trace_m4_7["mu"], color="k")
+ax.set_xlabel("year")
+ax.set_ylabel("days in year")
